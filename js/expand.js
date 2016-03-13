75c975405452e77d094b3964cbb268a38523662e
Udaiveer <uda4000@hotmail.com>
2016-03-13 01:54:39 -0800

    -m removed extra css fiel

--- js/expand.js
+++ js/expand.js
@@ -1,23 +1,6 @@
 $(document).ready(function() {
+  $("#time-link").trigger('click');
 	$("#numbers-link").trigger('click');
 	$("#search-link").trigger('click');
-
-  // (function(){
-  //   $(".math").each(function() {
-  //     var texTxt = $(this).text();
-  //     el = $(this).get(0);
-  //     if(el.tagName == "DIV"){
-  //         addDisp = "\\displaystyle";
-  //     } else {
-  //         addDisp = "";
-  //     }
-  //     try {
-  //         katex.render(addDisp+texTxt, el);
-  //     }
-  //     catch(err) {
-  //         $(this).html("<span class='err'>"+err);
-  //     }
-  //   }); 
-  // })(); 
 })
 
