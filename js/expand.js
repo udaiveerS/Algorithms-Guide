70d87e02db016bb59f9ef4a229ba9a8352d4ccd6
Udaiveer <uda4000@hotmail.com>
2016-03-13 01:32:09 -0800

    finished one big O for loop problem

--- js/expand.js
+++ js/expand.js
@@ -2,22 +2,22 @@ $(document).ready(function() {
 	$("#numbers-link").trigger('click');
 	$("#search-link").trigger('click');
 
-  (function(){
-    $(".math").each(function() {
-      var texTxt = $(this).text();
-      el = $(this).get(0);
-      if(el.tagName == "DIV"){
-          addDisp = "\\displaystyle";
-      } else {
-          addDisp = "";
-      }
-      try {
-          katex.render(addDisp+texTxt, el);
-      }
-      catch(err) {
-          $(this).html("<span class='err'>"+err);
-      }
-    }); 
-  })(); 
+  // (function(){
+  //   $(".math").each(function() {
+  //     var texTxt = $(this).text();
+  //     el = $(this).get(0);
+  //     if(el.tagName == "DIV"){
+  //         addDisp = "\\displaystyle";
+  //     } else {
+  //         addDisp = "";
+  //     }
+  //     try {
+  //         katex.render(addDisp+texTxt, el);
+  //     }
+  //     catch(err) {
+  //         $(this).html("<span class='err'>"+err);
+  //     }
+  //   }); 
+  // })(); 
 })
 
