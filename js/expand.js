e0f187d4e5449b6c3d5a60133bbc66cb1ccef002
Udaiveer <uda4000@hotmail.com>
2016-03-14 04:45:49 -0700

    added string problem

--- js/expand.js
+++ js/expand.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
-  $("#time-link").trigger('click');
-	$("#numbers-link").trigger('click');
-	$("#search-link").trigger('click');
+ 	// $("#time-link").trigger('click');
+	// $("#numbers-link").trigger('click');
+	// $("#search-link").trigger('click');
+	// $("#strings-link").trigger('click');
+
+	$('.problem-category').trigger('click');
 })
 
